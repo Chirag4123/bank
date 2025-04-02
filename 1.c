e19de9e09a7b682e0e18d6efadc19d145b55b9b5
Chirag4123 <chiruchirag100@gmail.com>
2025-04-02 10:26:20 +0530

    Update 1.c
    top

--- 1.c
+++ 1.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() 
-{
-    printf("Hello, World!\n");
-    return 0;
-}
+int main() {
+   
+    int totalQuestions = 75;
+    int cppQuestions = 10;
+    int cQuestions = 30;
+    int javaQuestions = 35;
+
+
+    float cppCorrectPercentage = 0.70f;
+    float cCorrectPercentage = 0.40f;
+    float javaCorrectPercentage = 0.60f;
